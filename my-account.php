8a2b8d76adb47e69e3d37de764e9b877627c8ed9
Aleksandra Gefert <aleksandragefert@gmail.com>
2017-08-28 13:33:16 +0200

    upload zdjec

---
+++ my-account.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ideo5
+ * Date: 28.08.2017
+ * Time: 13:02
+ */
+if (isset($_POST['uploadprofileimg'])) {
+    $image = base64_encode(file_get_contents($_FILES['profileimg']['tmp_name']));
+    $options = array('http'=>array(
+        'method'=>"POST",
+        'header'=>"Authorization: Bearer b61fd72781aad421df507a86e03f4d27fc734356\n".
+            "Content-Type: application/x-www-form-urlencoded",
+        'content'=>$image
+    ));
+    $context = stream_context_create($options);
+    $imgurURL = "https://api.imgur.com/3/image";
+    $response = file_get_contents($imgurURL, false, $context);
+}
+?>
+<h1>My Account</h1>
+<form action="my-account.php" method="post" enctype="multipart/form-data">
+    Upload a profile image:
+    <input type="file" name="profileimg">
+    <input type="submit" name="uploadprofileimg" value="Upload Image">
+</form>
