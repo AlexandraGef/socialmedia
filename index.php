a6980f44e6a6a22be8461cc0c5b134e1d325ddb3
Aleksandra Gefert <aleksandragefert@gmail.com>
2017-08-28 12:12:20 +0200

    timeline

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ideo5
- * Date: 23.08.2017
- * Time: 12:48
- */
 include('./classes/DB.php');
 include('./classes/Login.php');
 include('./classes/Post.php');
@@ -13,7 +7,7 @@ if (Login::isLoggedIn()) {
     $userid = Login::isLoggedIn();
     $showTimeline = True;
 } else {
-    echo 'Jesteś niezalogowany !';
+    echo 'Not logged in';
 }
 if (isset($_GET['postid'])) {
     Post::likePost($_GET['postid'], $userid);
@@ -21,17 +15,17 @@ if (isset($_GET['postid'])) {
 $followingposts = DB::query('SELECT posts.id, posts.body, posts.likes, users.`username` FROM users, posts, followers
 WHERE posts.user_id = followers.user_id
 AND users.id = posts.user_id
-AND follower_id = 1
+AND follower_id = 2
 ORDER BY posts.likes DESC;');
 foreach($followingposts as $post) {
     echo $post['body']." ~ ".$post['username'];
     echo "<form action='index.php?postid=".$post['id']."' method='post'>";
     if (!DB::query('SELECT post_id FROM post_likes WHERE post_id=:postid AND user_id=:userid', array(':postid'=>$post['id'], ':userid'=>$userid))) {
-        echo "<input type='submit' name='like' value='Lubie'>";
+        echo "<input type='submit' name='like' value='Like'>";
     } else {
-        echo "<input type='submit' name='unlike' value='Nie lubie'>";
+        echo "<input type='submit' name='unlike' value='Unlike'>";
     }
-    echo "<span>".$post['likes']." polubień</span>
+    echo "<span>".$post['likes']." likes</span>
         </form>
         <hr /></br />";
 }
